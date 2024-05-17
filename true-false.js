1911be2c4f1b4aad264653327b5f231af9a37eb6
Emran <mdshojibkhan934@gmail.com>
2024-05-17 22:36:39 +0600

    add

---
+++ true-false.js
@@ -0,0 +1,18 @@
+// falsy
+// false
+// 0
+// ""
+// undefined
+// null
+// NaN
+// Truthy
+// "0" " " [] 
+let name = NaN;
+console.log(name);
+
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("codition is false");
+}
